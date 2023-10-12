8463914447b2c901b83ba48a071d865f11a9ca47
Mohammad Tasfiq Jawaad <108393918+Tasfiq-Jawaad@users.noreply.github.com>
2023-10-12 14:21:16 +0000

    starting practice (12/10/23)

---
+++ week2/add two num.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+    float num1, num2;
+    printf("Enter two numbers\n");
+    scanf("%f%f", &num1, &num2);
+    printf("Sum of %f and %f is %f\n", num1, num2, num1+num2);
+}
---
+++ week2/favAnimal.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+void main(){
+    printf("What is your favourite animal?\n");
+    char animal[100];
+    scanf("%s", animal);
+    printf("So your fav animal is %s\n", animal);
+}
