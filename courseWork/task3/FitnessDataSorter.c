f7c16288c92d87f7eb963a4bfe35ec81d57add18
Mohammad Tasfiq Jawaad <108393918+Tasfiq-Jawaad@users.noreply.github.com>
2023-12-11 18:39:07 +0000

    reading file - debug

--- courseWork/task3/FitnessDataSorter.c
+++ courseWork/task3/FitnessDataSorter.c
@@ -25,7 +25,44 @@ void tokeniseRecord(char *record, char delimiter, char *date, char *time, int *s
     }
 }
 
+FILE *open_file(char filename[], char mode[]) {
+    FILE *file = fopen(filename, mode);
+    if (file == NULL) {
+        perror("");
+        exit(1);
+    }
+    return file;
+}
+
+int checkFile(char line_buffer[], int buffer_size, FILE *file){
+    int dataCounter = 0;
+    while (fgets(line_buffer, buffer_size, file) != NULL)
+    {
+        //collecting the total number of record
+        dataCounter++;
+
+        char tempDate[11];
+        char tempTime[6];
+        int tempSteps[1];
+
+        tokeniseRecord(line_buffer, ',', tempDate, tempTime, tempSteps);
+        // printf("date: %s\ntime: %s\nsteps: %ls\n", tempDate, tempTime, tempSteps2);
+        printf("date: %s\ntime: %s\nsteps: %d", tempDate, tempTime, tempSteps[0]);
+        printf("\n");
+    }
+}
 
 int main() {
-    qsort();
+    printf("Enter filename: ");
+    char filename [100];
+    // scanf("%s", filename);
+    FILE *file = open_file("FitnessData_2023.csv", "r"); // change the fine name
+
+    int buffer_size = 100;
+    char line_buffer[buffer_size];
+
+    checkFile(line_buffer, buffer_size, file);
+
+    fclose(file);
+    return 0;
 }
